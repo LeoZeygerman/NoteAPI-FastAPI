a34b9d11e13b0d44f3037c5af2893fe0ecc820ef
BBYBOI <zeygermanleo@gmail.com>
2026-09-03 19:14:31 +0500

    create database

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+engine = create_async_engine("sqlite+aiosqlite:///test.db")
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_session():
+    async with new_session() as session:
+        yield session
+
+Session = Annotated[AsyncSession, Depends(new_session)]
