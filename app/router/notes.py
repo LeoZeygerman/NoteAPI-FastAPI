a9b263886aa483c2a4f7bf7decf960d5ae1ed0da
BBYBOI <zeygermanleo@gmail.com>
2026-09-05 16:01:37 +0500

    add delete and patch

--- app/router/notes.py
+++ app/router/notes.py
@@ -1,14 +1,76 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from sqlalchemy import select
 from database import SessionDep
-from app.schemas import NoteCreate
+from app.schemas import NoteCreate, ResponseNote, UpdateNote
 from app.models import NoteOrm
 
 router = APIRouter(prefix='/notes', tags=['Notes'])
 
-@router.post('/')
+@router.post('/', summary='Добавить заметку', response_model=ResponseNote)
 async def create_note(session: SessionDep, note: NoteCreate):
+    
     new_note = NoteOrm(title = note.title, content = note.content)
-
     session.add(new_note)
-    session.commit()
-    return {'msg': f'Задача {note.title} успешно добавлена!'}
+    await session.commit()
+
+    return {'msg': f'Заметка {note.title} успешно добавлена!'}
+
+
+@router.get('/{note_id}', summary='Получить одну заметку по id', response_model=ResponseNote)
+async def get_one_by_id(session: SessionDep, note_id: int):
+
+    query = select(NoteOrm).where(NoteOrm.id == note_id)
+    note = await session.execute(query)
+    result = note.scalar_one_or_none()
+
+    if result is None:
+        raise HTTPException(status_code=404, detail='Заметка не найдена!')
+
+    return result
+
+
+@router.get('/', summary='Получить все заметки', response_model=list[ResponseNote])
+async def get_all(session: SessionDep):
+
+    query = select(NoteOrm)
+    note = await session.execute(query)
+    result = note.scalar().all()
+
+    return result
+
+
+@router.patch('/{note_id}', summary='Изменить заметку', response_model=ResponseNote)
+async def edit_note(session: SessionDep, note_id: int, note: UpdateNote):
+
+    query = select(NoteOrm).where(NoteOrm.id == note_id)
+    result = await session.execute(query)
+    db_notes = result.scalar_one_or_none()
+
+    if db_notes is None:
+        raise HTTPException(status_code=404, detail='Заметка не найдена!')
+
+    if note.title is not None:
+        db_notes.title = note.title
+    if note.content is not None:
+        db_notes.content = note.content
+
+    await session.commit()
+    return db_notes
+
+
+@router.delete('/{note_id}', summary='Удалить заметку')
+async def delete_note(session: SessionDep, note_id: int):
+
+    query = select(NoteOrm).where(NoteOrm.id == note_id)
+    result = await session.execute(query)
+    note = result.scalar_one_or_none()
+
+    if note is None:
+        raise HTTPException(status_code=404, detail='Заметка не найдена!')
+
+    await session.delete(note)
+    await session.commit()
+
+    return {'msg': f'Заметка {note.title} удалена!'}
+
+
